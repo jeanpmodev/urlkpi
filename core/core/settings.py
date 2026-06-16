6bb5ea1ec6f932c0b796eeefd4ff374d91c556c0
jeanpmodev <jeanpmodev@gmail.com>
2026-06-16 08:33:56 -0300

    enabling paths of middle-earth

--- core/core/settings.py
+++ core/core/settings.py
@@ -50,6 +50,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'dash.middleware.MordorMiddleEarthMiddleware', 
 ]
 
 ROOT_URLCONF = 'core.urls'
